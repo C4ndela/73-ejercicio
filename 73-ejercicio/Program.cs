bd04e5bcce617541a4de08a7ebe23cdb86f889a6
c4ndela <ds21tn.contreras.candela@gmail.com>
2022-10-13 21:28:41 -0300

    Agregar archivos de proyecto.

---
+++ 73-ejercicio/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _73_ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string palabras;
+            Queue <string> cola = new Queue<string>();
+
+            Console.Write("Ingrese una cadena: ");
+            palabras = Console.ReadLine();  
+
+
+            while (palabras != string.Empty)
+            {
+                cola.Enqueue(palabras);
+                Console.Write("Ingrese la proxima cadena: ");
+                palabras = Console.ReadLine();
+            }
+            while (cola.Count >0)
+            {
+                Console.WriteLine((string) cola.Dequeue()); 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
